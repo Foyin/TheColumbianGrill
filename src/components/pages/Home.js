51065e1867f10ecead198f50b2f92d433363bd35
Foyin Ogbara <foyinogb@live.co.uk>
2020-02-10 22:01:07 -0500

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -35,7 +35,7 @@ function LandingMainLayout(props){
         <img 
           width='400vw'
           height='500vh'
-          src='../../images/cgmenu.jpg'
+          src='images/cgmenu.jpg'
         />
       </div>
   ); 
